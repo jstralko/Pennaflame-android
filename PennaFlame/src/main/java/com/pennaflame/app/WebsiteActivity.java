095b42f72760acd69fbbfbd3b96eab69005d633c
Jerry Stralko <gerald.stralko@lexi.com>
2013-10-30 14:02:47 -0400

    cleanup imports

--- PennaFlame/src/main/java/com/pennaflame/app/WebsiteActivity.java
+++ PennaFlame/src/main/java/com/pennaflame/app/WebsiteActivity.java
@@ -1,15 +1,11 @@
 package com.pennaflame.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.webkit.WebView;
 
 public class WebsiteActivity extends ActionBarActivity {
